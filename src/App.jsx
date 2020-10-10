a416d858535086f779dc790aa0ca31ec8fdfd16f
greyewi1 <greyewi@gmail.com>
2020-10-10 16:04:00 +0300

    add Story components

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, {Fragment} from 'react'
+import 'App.scss'
+import {Route, Switch} from 'react-router-dom'
+import { Header } from 'components/Layout/Header'
+import Layout from 'components/Layout'
+import Games from 'components/Games'
+import Story from 'components/Layout/Story'
+
+const App = () => {
+
+  return (
+    <Fragment>
+      <Header/>
+      <Layout>
+        <Switch>
+          <Route path="/:gameName" component={Story}/>
+          <Route path="/" component={Games}/>
+        </Switch>
+      </Layout>
+    </Fragment>
+  )
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import store from './redux/store'
+import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'connected-react-router/immutable'
+import history from './history'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { purple, lightBlue } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[700],
+    },
+    secondary: {
+      main: lightBlue['A400'],
+    },
+    error: {
+      main: "#ff604f"
+    }
+  },
+})
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <ThemeProvider theme={theme}>
+        <App/>
+      </ThemeProvider>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
