bf5a12a91bc41dc6fe38791b41febe34142c1a13
greyewi1 <greyewi@gmail.com>
2020-11-10 19:28:55 +0200

    fix idea

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,21 +3,13 @@ import { connectRouter } from 'connected-react-router'
 import { reducer as form } from 'redux-form'
 import history from '../history'
 
-import charactersReducer, {
-  moduleName as charactersModule,
-} from '../ducks/characters'
+import charactersReducer, { moduleName as charactersModule } from '../ducks/characters'
 import gamesReducer, { moduleName as gamesModule } from '../ducks/games'
-import locationsReducer, {
-  moduleName as locationsModule,
-} from '../ducks/locations'
+import locationsReducer, { moduleName as locationsModule } from '../ducks/locations'
 import eventsReducer, { moduleName as eventsModule } from '../ducks/events'
-import characteristicReducer, {
-  moduleName as characteristicModule,
-} from '../ducks/characteristic'
+import characteristicReducer, { moduleName as characteristicModule } from '../ducks/characteristic'
 import itemsReducer, { moduleName as itemsModule } from '../ducks/items'
-import drawReducer, {
-  moduleName as drawModule,
-} from '../shared/ui/Drawer/drawDuck'
+import drawReducer, { moduleName as drawModule } from '../shared/ui/Drawer/drawDuck'
 
 export default combineReducers({
   form,
